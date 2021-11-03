300b7bcd903229dfac71f1033f777a3ca1bb642f
“isab554b” <isab554b@stud.kea.dk>
2021-11-03 13:13:33 +0100

    footer

--- site-info.php
+++ site-info.php
@@ -8,5 +8,10 @@
 ?>
 
 <div class="site-info">
+	<?php echo wp_kses_post( html_entity_decode( get_theme_mod( 'footer_text', '' ) ) ); ?>
+	<?php
+	printf( esc_html__( 'Copyright' ) );
+	?>
+
 	
 </div><!-- .site-info -->
