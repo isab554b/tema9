e93cb1d60d95a8f68d79e3c9728ef49345e34b95
“isab554b” <isab554b@stud.kea.dk>
2021-11-03 12:57:05 +0100

    tekst i footer fjernet

--- site-info.php
+++ site-info.php
@@ -8,12 +8,5 @@
 ?>
 
 <div class="site-info">
-	<?php echo wp_kses_post( html_entity_decode( get_theme_mod( 'footer_text', '' ) ) ); ?>
 	
-	<?php esc_html_e( 'Theme by' ); ?>
-	<a href="<?php echo esc_url( __( 'https://keonthemes.com/', 'gutener-education' ) ); ?>" target="_blank">
-		<?php
-		printf( esc_html__( 'gruppe 1', 'gutener-education' ) );
-		?>
-	</a>
 </div><!-- .site-info -->
