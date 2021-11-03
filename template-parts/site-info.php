d9582c7a1a15fc52d30bad865f07f5d7654008c2
“isab554b” <isab554b@stud.kea.dk>
2021-11-03 13:16:09 +0100

    footer

--- template-parts/site-info.php
+++ template-parts/site-info.php
@@ -10,12 +10,8 @@
 <div class="site-info">
 	<?php echo wp_kses_post( html_entity_decode( get_theme_mod( 'footer_text', '' ) ) ); ?>
 	<?php
-	printf( esc_html__( 'Gutener Education', 'gutener-education' ) );
+	printf( esc_html__( 'Copyright' ) );
 	?>
-	<?php esc_html_e( 'Theme by', 'gutener-education' ); ?>
-	<a href="<?php echo esc_url( __( 'https://keonthemes.com/', 'gutener-education' ) ); ?>" target="_blank">
-		<?php
-		printf( esc_html__( 'Keon Themes', 'gutener-education' ) );
-		?>
-	</a>
+
+	
 </div><!-- .site-info -->
