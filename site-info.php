406790ad639965f2f964143cadd5080c9c7ed589
“isab554b” <isab554b@stud.kea.dk>
2021-11-04 13:40:05 +0100

    rettelse

--- site-info.php
+++ site-info.php
@@ -9,9 +9,7 @@
 
 <div class="site-info">
 	<?php echo wp_kses_post( html_entity_decode( get_theme_mod( 'footer_text', '' ) ) ); ?>
-	<?php
-	printf( esc_html__( 'Copyright' ) );
-	?>
+	
 
 	
 </div><!-- .site-info -->
